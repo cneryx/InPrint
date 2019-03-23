6091e3099e464869629754db5653b30a80bceee9
Jobo0 <jzhaojy@gmail.com>
2019-03-23 19:00:19 -0400

    added basic ui activity 2

--- InPrint/app/src/main/java/com/example/inprint/MainActivity.java
+++ InPrint/app/src/main/java/com/example/inprint/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.inprint;
 
         import android.content.Intent;
         import android.os.Bundle;
-        import android.support.annotation.NonNull;
-        import android.support.design.widget.BottomNavigationView;
         import android.support.v7.app.AppCompatActivity;
-        import android.view.MenuItem;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
@@ -21,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         //when user presses a button
-        Button button = (Button) findViewById(R.id.ipEnterButton);
+        Button button = (Button) findViewById(R.id.dataEnterButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Activity1.class);
