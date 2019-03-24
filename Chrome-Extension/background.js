1bacc80ff86f5ea01e9d2e322673c381f2fbc0ed
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-03-24 02:26:48 -0400

    Better site interactions

--- Chrome-Extension/background.js
+++ Chrome-Extension/background.js
@@ -2,27 +2,80 @@ var data = {greeting: 'hello!'};
 var siteData = {};
 var email = "";
 var password = "";
+var noEmail = false;
+var noPassword = false;
 
-fetch("http://127.0.0.1:5000/test.html", {
-    headers:{
-        'Content-Type': 'application/json'
-    }})
-    .then(function(response) {
-        return response.json();
+var host = window.location.hostname;
+
+getServerData();
+var constantLoop = setInterval(getServerData, 3000);
+
+function getServerData() {
+    fetch("http://127.0.0.1:5000/test.html", {
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-    .then(function(text) {
-        console.log(text);
-        siteData = text;
-        console.log(siteData)
-        setTimeout(killme, 1000);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (text) {
+            console.log(text);
+            siteData = text;
+            console.log(siteData)
+            setTimeout(fillInputs, 1000);
+        });
 
-function killme() {
+}
+
+function fillInputs() {
     console.log(siteData);
     console.log(siteData.facebook);
     email = siteData.facebook.email;
     password = siteData.facebook.password;
+    var canStop = false;
+    try {
+        if (host == "github.com") {
+            document.getElementById("login_field").value = email;
+        } else {
+            document.querySelector("input[type=\"email\"").value = email;
+        }
+        canStop = true;
+        noEmail = false;
+    } catch (e) {
+        noEmail = true;
+    }
 
-    document.getElementById("email").value = email;
-    document.getElementById("pass").value = password;
+    try {
+        if (window.location.href == "https://github.com/") {
+
+        } else {
+            document.querySelector("input[type=\"password\"").value = password;
+        }
+        canStop = true;
+        noPassword = false;
+    } catch (e) {
+        noPassword = true;
+    }
+
+    try {
+        setTimeout(autoPress, 100);
+        } catch (e) {
+
+    }
+
+    if (canStop || (noPassword && noEmail)) {
+        clearInterval(constantLoop);
+    }
+}
+
+function autoPress() {
+    try {
+        if (window.location.href == "https://github.com/login" && !noPassword) {
+            document.forms[0].submit();
+        }
+        document.forms["login_form"].submit();
+    } catch (e) {
+
+    }
 }
