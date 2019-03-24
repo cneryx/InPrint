62b272a704ab6c26e16d02504e75de5258f29312
Chris Xie <cx.chrisxie@gmail.com>
2019-03-24 03:56:17 -0400

    Connected app to server

---
+++ InPrint/app/src/main/java/com/example/inprint/BackgroundWorker.java
@@ -0,0 +1,64 @@
+package com.example.inprint;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+class BackgroundWorker extends AsyncTask<String, Void, Void> {
+    @Override
+    protected Void doInBackground(String... params) {
+
+        URL url = null;
+        try {
+            File saves = new File(Activity2.getAppcontext().getFilesDir(), "saves.txt");
+            Scanner scanner = new Scanner(new FileInputStream(saves));
+            String u = "http://395cfc6d.ngrok.io/send_logins?data=";
+            while (scanner.hasNextLine()) {
+                u += scanner.nextLine();
+            }
+            url = new URL(u);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner scanner = new Scanner(in);
+            System.out.println(scanner.nextLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+//            try {
+//                InputStream inputStream;
+//
+//                int status = urlConnection.getResponseCode();
+//
+//                if (status != HttpURLConnection.HTTP_OK)
+//                    inputStream = urlConnection.getErrorStream();
+//                else
+//                    inputStream = urlConnection.getInputStream();
+//                System.out.println(urlConnection.getResponseCode());
+////                Scanner scanner = new Scanner(inputStream);
+////                System.out.println(scanner.nextLine());
+//            } catch (IOException e) {
+//                e.printStackTrace();dw2rq
+//            }
+            urlConnection.disconnect();
+        }
+        System.out.println("sent");
+        return null;
+    }
+}
--- InPrint/app/src/main/java/com/example/inprint/FingerprintHandler.java
+++ InPrint/app/src/main/java/com/example/inprint/FingerprintHandler.java
@@ -1,23 +1,22 @@
 package com.example.inprint;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
-import android.Manifest;
 import android.os.CancellationSignal;
 import android.support.v4.app.ActivityCompat;
-import android.widget.ImageView;
 import android.widget.Toast;
-
-import pl.droidsonroids.gif.GifImageView;
 
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
     public boolean authenticed = false;
     private CancellationSignal cancellationSignal;
     private Context context;
+
     public FingerprintHandler(Context mContext) {
         context = mContext;
     }
+
     public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
         cancellationSignal = new CancellationSignal();
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
@@ -25,6 +24,7 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         }
         manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
     }
+
     public void onAuthenticationError(int errMsgId, CharSequence errString) {
 
 
@@ -32,21 +32,34 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         //Here, I’m creating the message that’ll be displayed if an error occurs//
 
         //0Toast.makeText(context, "Authentication error\n" + errString, Toast.LENGTH_LONG).show();
-    }@Override
-    public void onAuthenticationFailed() {
-       // Toast.makeText(context, "Authentication failed", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onAuthenticationFailed() {
+        // Toast.makeText(context, "Authentication failed", Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
-      //  Toast.makeText(context, "Authentication help\n" + helpString, Toast.LENGTH_LONG).show();
-    }@Override
+        //  Toast.makeText(context, "Authentication help\n" + helpString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
 
     //onAuthenticationSucceeded is called when a fingerprint has been successfully matched to one of the fingerprints stored on the user’s device//
     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
-        //Toast.makeText(context, "Success!", Toast.LENGTH_LONG).show();
-        authenticed = true;
+        if (Activity2.getAppcontext() != null) {
+            Toast.makeText(context, "Success!", Toast.LENGTH_LONG).show();
+            authenticed = true;
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.execute();
+        } else {
+            Toast.makeText(context, "Success!", Toast.LENGTH_LONG).show();
+            authenticed = true;
+        }
     }
-    public boolean isAuthenticed(){
+
+    public boolean isAuthenticed() {
         return authenticed;
     }
 }
