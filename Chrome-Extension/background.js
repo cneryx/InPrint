34de22a1c10aa7602b95320d23807576a55cd053
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-03-24 00:19:39 -0400

    More autofill

--- Chrome-Extension/background.js
+++ Chrome-Extension/background.js
@@ -1,5 +1,7 @@
 var data = {greeting: 'hello!'};
-var siteData;
+var siteData = {};
+var email = "";
+var password = "";
 
 fetch("http://127.0.0.1:5000/test.html", {
     headers:{
@@ -10,5 +12,17 @@ fetch("http://127.0.0.1:5000/test.html", {
     })
     .then(function(text) {
         console.log(text);
+        siteData = text;
+        console.log(siteData)
+        setTimeout(killme, 1000);
     });
 
+function killme() {
+    console.log(siteData);
+    console.log(siteData.facebook);
+    email = siteData.facebook.email;
+    password = siteData.facebook.password;
+
+    document.getElementById("email").value = email;
+    document.getElementById("pass").value = password;
+}
